9c406b727ce26b548796e12e36ef21a27dee467d
Arturo Santana <arturo.santtana@gmail.com>
2025-08-20 17:38:06 +0000

    🎉 First commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,9 @@
 const path = require("path");
-const glob = require("glob");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const INCLUDE_PATTERN =
-  /<include\s+src=["'](.+?)["']\s*\/?>\s*(?:<\/include>)?/gis;
-
-const processNestedHtml = (content, loaderContext, dir = null) =>
-  !INCLUDE_PATTERN.test(content)
-    ? content
-    : content.replace(INCLUDE_PATTERN, (m, src) => {
-        const filePath = path.resolve(dir || loaderContext.context, src);
-        loaderContext.dependency(filePath);
-        return processNestedHtml(
-          loaderContext.fs.readFileSync(filePath, "utf8"),
-          loaderContext,
-          path.dirname(filePath),
-        );
-      });
-
-// HTML generation
-const paths = [];
-const generateHTMLPlugins = () =>
-  glob.sync("./src/*.html").map((dir) => {
-    const filename = path.basename(dir);
-
-    if (filename !== "404.html") {
-      paths.push(filename);
-    }
-
-    return new HtmlWebpackPlugin({
-      filename,
-      template: `./src/${filename}`,
-      favicon: `./src/images/favicon.ico`,
-      inject: "body",
-    });
-  });
-
 module.exports = {
-  mode: "development",
+  mode: "development", // o "production"
   entry: "./src/js/index.js",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "./build"),
-    },
-    compress: true,
-    port: 3000,
-    hot: true,
-  },
   module: {
     rules: [
       {
@@ -55,9 +11,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
+          options: { presets: ["@babel/preset-env"] },
         },
       },
       {
@@ -69,46 +23,32 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [
-                  require("autoprefixer")({
-                    overrideBrowserslist: ["last 2 versions"],
-                  }),
-                ],
+                plugins: [require("autoprefixer")()],
               },
             },
           },
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: "asset/resource",
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        test: /\.(woff2?|eot|ttf|otf)$/i,
         type: "asset/resource",
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-        options: {
-          preprocessor: processNestedHtml,
-        },
       },
     ],
   },
   plugins: [
-    ...generateHTMLPlugins(),
     new MiniCssExtractPlugin({
       filename: "style.css",
-      chunkFilename: "style.css",
     }),
   ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
     clean: true,
-    assetModuleFilename: "[path][name][ext]",
+    assetModuleFilename: "assets/[name][ext]",
   },
-  target: "web", // fix for "browserslist" error message
-  stats: "errors-only", // suppress irrelevant log messages
+  stats: "errors-only",
 };
